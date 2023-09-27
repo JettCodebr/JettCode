a28a566be4d0d52924845cb142211d07767d1020
Kvcss <kaiovsilva60@gmail.com>
2023-09-27 03:27:47 -0300

    #feature01 - Inicio do Projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className = "container">
+      <h1 className='Title'>Teste</h1>
+    </div>
+  );
+}
+
+export default App;
